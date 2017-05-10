ad07f5b0617a55571044602d8e40bc651e35997a
Do Van Hung <vanhungpc@gmail.com>
2017-05-10 16:21:19 +0700

    update momo sdk

---
+++ momo_partner_sdk/src/main/java/vn/momo/momo_partner/MoMoParameterNamePayment.java
@@ -0,0 +1,16 @@
+package vn.momo.momo_partner;
+
+/**
+ * Created by hungdo on 5/9/17.
+ */
+
+//todo parameter payment momo
+public interface MoMoParameterNamePayment {
+    String MERCHANT_CODE = "merchantcode";
+    String MERCHANT_NAME = "merchantname";
+    String MERCHANT_NAME_LABEL = "merchantnamelabel";
+    String AMOUNT = "amount";
+    String FEE = "fee";
+    String DESCRIPTION = "description";
+    String EXTRA = "extra";
+}
